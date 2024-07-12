df922bffd2cbc5413c8b9baa7f771f5d892591f8
farish-jamal <farishjamal98@gmail.com>
2024-07-12 21:26:10 +0530

    Home Page added

---
+++ frontend/src/components/JobCards.vue
@@ -0,0 +1,46 @@
+<script setup>
+import JobCard from "@/components/JobCard.vue";
+</script>
+
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <!-- Job Listing 1 -->
+        <JobCard
+          opportunity="Full-Time"
+          role="Senior Vue Developer"
+          description="
+                We are seeking a talented Front-End Developer to join our team
+                in Boston, MA. The ideal candidate will have strong skills in
+                HTML, CSS, and JavaScript..."
+          salary="80K "
+          location="Boston, MA"
+        />
+        <JobCard
+          opportunity="Full-Time"
+          role="Senior Vue Developer"
+          description="
+                We are seeking a talented Front-End Developer to join our team
+                in Boston, MA. The ideal candidate will have strong skills in
+                HTML, CSS, and JavaScript..."
+          salary="80K "
+          location="Boston, MA"
+        />
+        <JobCard
+          opportunity="Full-Time"
+          role="Senior Vue Developer"
+          description="
+                We are seeking a talented Front-End Developer to join our team
+                in Boston, MA. The ideal candidate will have strong skills in
+                HTML, CSS, and JavaScript..."
+          salary="80K "
+          location="Boston, MA"
+        />
+      </div>
+    </div>
+  </section>
+</template>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,9 +1,13 @@
 <script setup>
  import Hero from "@/components/Hero.vue";
  import HomeCards from "@/components/HomeCards.vue";
+ import JobCards from "@/components/JobCards.vue";
+ import Button from "@/components/Button.vue";
 </script>
 
 <template>
  <Hero title="Become a Vue Dev" subTitle="Find the Vue job that fits your skills and needs"/>
  <HomeCards />
+ <JobCards />
+ <Button />
 </template>
